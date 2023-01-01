46d68c0a546a32db8dc0e071a4d3afe4a39ec974
AlexS <ashilov84@ya.ru>
2023-01-01 17:13:08 +0500

    Выполнил задание к уроку №5

--- script52/Comment.php
+++ script52/Comment.php
@@ -1,6 +1,30 @@
 <?php
+// Реализуйте два класса: Comment и TaskService. Comment должен содержать свойства author (User), task (Task) и
+// text (string). TaskService должен реализовывать статичный метод addComment(User, Task, text), добавляющий к задаче
+// комментарий пользователя. Отношение между классами задачи и комментария должны быть построены по типу ассоциация,
+// поэтому необходимо добавить соответствующее свойство и методы классу Task.
 
 class Comment
 {
+    private User $author;
+    private string $text;
 
+    public function __construct(User $user, string $text)
+    {
+        $this->author = $user;
+        $this->text = $text;
+    }
+
+    public function getAuthor () : User
+    {
+        return $this->author;
+    }
+    public function getText () : string
+    {
+        return $this->text;
+    }
+    public function getComment () : array
+    {
+        return [$this->author, $this->text];
+    }
 }
--- script53/Product.php
+++ script53/Product.php
@@ -2,5 +2,76 @@
 
 class Product
 {
+    private string $title;
+    private float $price;
+    private ?array $components;
 
+    public function __construct(string $title, float | array $i)
+    {
+        $this->title = $title;
+
+        if(is_float($i)){
+            $this->price = $i;
+        }elseif(is_array($i)){
+            $this->components = $i;
+            $this->setPriceComponents();
+        }
+    }
+
+    public function getTitle () : string
+    {
+        return $this->title;
+    }
+    public function getPrice () :float
+    {
+        return $this->price;
+    }
+    public function getComponents () : ?array
+    {
+        return $this->components;
+    }
+    private function getSumPriceComponents (array $arr) : float
+    {
+        $price = 0;
+        foreach($arr as $product)
+        {
+            $price +=  $product->price;
+        }
+        return $price;
+    }
+
+    public function setTitle (string $title) : void
+    {
+        $this->title = $title;
+    }
+    public function setPrice (float $price) : void
+    {
+        $this->price = $price;
+    }
+    public function setPriceComponents () : void
+    {
+        $price = 0;
+        foreach($this->components as $product)
+        {
+            $price +=  $product->price;
+        }
+        $this->price = $price;
+
+    }
+    public function addComponent (Product $product) : void
+    {
+        $this->components[] = $product;
+        $this->setPriceComponents();
+    }
+    public function removeComponent (string $title) : void
+    {
+        foreach ($this->components as $key => $component)
+        {
+            if($component->title === $title) {
+                array_splice($this->components, $key, 1);
+                break;
+            }
+        }
+        $this->setPriceComponents();
+    }
 }
